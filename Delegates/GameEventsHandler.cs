fe206f236eb3afb9301ff286a595c7ec24cb6ed5
LuanRoger <luan.roger.2003@gmail.com>
2023-11-01 18:59:50 -0300

    feat: separates events delegates methods

---
+++ Delegates/GameEventsHandler.cs
@@ -0,0 +1,82 @@
+﻿using JaguarGame.BoardRenderer;
+using JaguarGame.Models.EventsArgs;
+using JaguarGame.Models.Interfaces;
+using Spectre.Console;
+using Spectre.Console.Rendering;
+
+namespace JaguarGame.Delegates;
+
+public class GameEventsHandler
+{
+    private readonly LiveDisplayContext _displayContext;
+    private readonly Layout _layout;
+
+    public GameEventsHandler(LiveDisplayContext displayContext, Layout layout)
+    {
+        _displayContext = displayContext;
+        _layout = layout;
+    }
+
+    public void UpdateEventsOnMove(object sender, MoveEventArgs eventArgs)
+    {
+        const string moveTextFormat = "{0} foi para {1} ({2})";
+        AdugoBoardRenderer boardRenderer = new(5, 5);
+        _layout["Eventos"].Update(new Panel(
+                new Markup(string.Format(moveTextFormat, 
+                    eventArgs.player.name, eventArgs.move.newPoss.id, eventArgs.moveScore)))
+            .Header("Eventos"));
+        _layout["CurrentGame"].Update(
+            new Panel(
+                new Align(
+                    boardRenderer.Render(eventArgs.boardAfterMove),
+                    HorizontalAlignment.Center, VerticalAlignment.Middle))
+                .Expand());
+        _displayContext.Refresh();
+    }
+    
+    public void UpdateEventsOnStartRound(object sender, StartRoundEventArgs eventArgs)
+    {
+        IEnumerable<IRenderable> jaguarMovesRender = eventArgs.jaguarMoves
+            .Select(move => new Markup($"Jaguar [bold]-> {move.newPoss.id}[/]"));
+        _layout["PossibleMoves"].Update(new Panel(
+                new Rows(jaguarMovesRender))
+            .Header("Movimentos possiveis")
+            .Expand()
+        );
+        
+        _displayContext.Refresh();
+    }
+    
+    public void UpdateRoundInfos(object sender, RoundEndedEventArgs eventArgs)
+    {
+        Style ruleStyle = new Style()
+            .Foreground(Color.Gold1);
+            
+        _layout["Data"].Update(
+            new Panel(
+                    new Rows(
+                        new BarChart()
+                            .AddItem("Round", eventArgs.round, Color.CadetBlue)
+                            .AddItem("Quantidade moviemtnos", eventArgs.movesCount, Color.DarkSlateGray1)
+                            .AddItem("Cachorro vivos", eventArgs.dogsAliveCount, Color.LightSalmon3_1)
+                            .WithMaxValue(30)
+                            .RightAlignLabel(),
+                        new Markup($"Posição da onça: {eventArgs.jagPoss.id}"),
+                        new Rule("[bold yellow]Relação de movimento[/]")
+                            .RuleStyle(ruleStyle)
+                            .Centered(),
+                        new BreakdownChart()
+                            .AddItem("Jaguar", eventArgs.boardEvaluation.jaguarChangeToWin, Color.Silver)
+                            .AddItem("Dogs", eventArgs.boardEvaluation.dogsChangeToWin, Color.Gold1)
+                            .ShowPercentage()
+                            .UseValueFormatter(d =>
+                            {
+                                double percentage = d * 100;
+                                return $"{percentage:0.00}%";
+                            })
+                    ))
+                .Header("Informações do ultimo round")
+                .Expand()
+        );
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
 ﻿using JaguarGame;
-using JaguarGame.BoardDefinitions;
 using JaguarGame.BoardDefinitions.Definitions;
-using JaguarGame.BoardRenderer;
-using JaguarGame.Models;
-using JaguarGame.Models.EventsArgs;
+using JaguarGame.Delegates;
 using Spectre.Console;
-using Spectre.Console.Rendering;
 
 const string DEFAULT_JAGUAR_PLAYER_NAME = "Jaguar";
 const string DEFAULT_DOGS_PLAYER_NAME = "Dogs";
@@ -19,69 +15,27 @@ Game game = new(new(jaguarPlauyerName), new(dogsPlayerName), new AdugoBoard());
 
 Layout mainLayout = new Layout("Root")
     .SplitColumns(
-        new Layout("Preview"),
+        new Layout("Previews")
+            .SplitRows(
+                new Layout("CurrentGame"),
+                new Layout("PossibleMoves")),
         new Layout("Infos")
-            .SplitRows(new Layout("Data"),
+            .SplitRows(
+                new Layout("Data"),
                 new Layout("Eventos"))
         );
 
 AnsiConsole.Live(mainLayout)
     .Start(ctx =>
     {
-        game.OnMove += UpdateEventsOnMove;
-        game.OnRoundEnded += UpdateRoundInfos;
+        GameEventsHandler eventsHandler = new(ctx, mainLayout);
+        game.OnMove += eventsHandler.UpdateEventsOnMove;
+        game.OnRoundEnded += eventsHandler.UpdateRoundInfos;
+        game.OnStartRound += eventsHandler.UpdateEventsOnStartRound;
         var winner = game.Start();
         mainLayout["Eventos"].Update(new Panel(
                 new Markup($"O vencedor é {winner?.ToString() ?? "Ninguém"}"))
             .Header("Eventos"));
-        return;
-
-        void UpdateEventsOnMove(object sender, MoveEventArgs eventArgs)
-        {
-            const string moveTextFormat = "{0} foi para {1} ({2})";
-            AdugoBoardRenderer boardRenderer = new(5, 5);
-            mainLayout["Eventos"].Update(new Panel(
-                new Markup(string.Format(moveTextFormat, 
-                    eventArgs.player.name, eventArgs.move.newPoss.id, eventArgs.moveScore)))
-                .Header("Eventos"));
-            mainLayout["Preview"].Update(
-                new Panel(
-                    new Align(
-                        boardRenderer.Render(eventArgs.boardAfterMove),
-                        HorizontalAlignment.Center, VerticalAlignment.Middle)));
-            ctx.Refresh();
-        }
-        void UpdateRoundInfos(object sender, RoundEndedEventArgs eventArgs)
-        {
-            Style ruleStyle = new Style()
-                .Foreground(Color.Gold1);
-            
-            mainLayout["Data"].Update(
-                new Panel(
-                    new Rows(
-                        new Rule("[bold blue]Informações do ultimo round[/]")
-                            .RuleStyle(ruleStyle)
-                            .Centered(),
-                        new Markup($"Round: {eventArgs.round}"),
-                        new Markup($"Posição da onça: {eventArgs.jagPoss.id}"),
-                        new Markup($"Cachorros vivos: {eventArgs.dogsAliveCount}"),
-                        new Rule("[bold yellow]Chance de vitória[/]")
-                            .RuleStyle(ruleStyle)
-                            .Centered(),
-                        new BreakdownChart()
-                            .AddItem("Jaguar", eventArgs.boardEvaluation.jaguarChangeToWin, Color.Silver)
-                            .AddItem("Dogs", eventArgs.boardEvaluation.dogsChangeToWin, Color.Gold1)
-                            .ShowPercentage()
-                            .UseValueFormatter(d =>
-                            {
-                                double percentage = d * 100;
-                                return $"{percentage:0.00}%";
-                            })
-                    ))
-                    .Header("Informações")
-                    .Expand()
-                );
-        }
     });
 
 AnsiConsole.Write(mainLayout);
